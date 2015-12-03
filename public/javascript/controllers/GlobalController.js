aca871459c1b5bacf8d8f2044aeed4597f373adf
Jeremy Doucet <jeremy.doucet93@gmail.com>
2015-12-03 14:43:45 -0800

    users are now logged in after refreshing the page

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -4,20 +4,16 @@
   function GlobalController(UserFactory, $state) {
       var vm = this;
       vm.user = {};
-      vm.isLoggedIn = false;
+      vm.status = UserFactory.status;
 
       vm.register = function() {
         UserFactory.register(vm.user).then(function(res) {
-          alert('Logged in!');
-          vm.isLoggedIn = true;
           $state.go('Home');
         });
       };
 
       vm.login = function() {
         UserFactory.login(vm.user).then(function(res) {
-          alert('logged in!');
-          vm.isLoggedIn = true;
           $state.go('Home');
         });
       };
--- public/javascript/services/UserFactory.js
+++ public/javascript/services/UserFactory.js
@@ -3,6 +3,7 @@
   angular.module('app').factory('UserFactory', UserFactory);
   function UserFactory($http, $q, $window) {
     var o = { };
+    o.status = {};
 
     o.register = function(user) {
       var q = $q.defer();
@@ -28,11 +29,22 @@
 
     o.setToken = function(token) {
       $window.localStorage.setItem('token', token);
+      o.setUser();
     };
 
     o.removeToken = function() {
       $window.localStorage.removeItem('token');
+      o.status._id = null;
+      o.status.email = null;
     };
+
+    o.setUser = function() {
+      var token = JSON.parse(atob(o.getToken().split('.')[1]));
+      o.status._id = token._id;
+      o.status.email = token.email;
+    }
+
+    if(o.getToken()) o.setUser();
 
     return o;
   }
